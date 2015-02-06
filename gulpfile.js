3e665acca405f02fc2c6442935b64502b72fb84f
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-02-06 09:48:06 +0200

    Added separate gulp builds for libs/files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-var gulp = require('gulp');
-var react = require('gulp-react');
+var gulp         = require('gulp');
+var browserify   = require('browserify');
+var source       = require('vinyl-source-stream');
 
-gulp.task('react-components', function () {
-	return gulp.src('front/**/*.jsx')
-		.pipe(react())
-		.pipe(gulp.dest('front/build'));
+var dependencies = [ "lodash", "react", "react-router" ];
+
+gulp.task('libs', function () {
+	return browserify()
+		.require(dependencies)
+		.bundle()
+		.pipe(source('libs.js'))
+		.pipe(gulp.dest('./front'));
 });
 
-gulp.task('default', ['react-components']);
+gulp.task('scripts', function () {
+	return browserify('./front/app.js')
+		.external(dependencies)
+		.bundle()
+		.pipe(source('build.js'))
+		.pipe(gulp.dest('./front'));
+});
+
+gulp.task('default', ['scripts']);
