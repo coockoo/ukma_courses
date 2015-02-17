a2d1ee6de4b38d316ccefc69f1bf3482adacbcc2
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-02-17 11:12:32 +0200

    UC-8 Added comments table

--- postgres/tables.sql
+++ postgres/tables.sql
@@ -1,4 +1,5 @@
 \i ./tables/courses.sql
 \i ./tables/users.sql
 \i ./tables/ratings.sql
+\i ./tables/comments.sql
 \i ./tables/courses_ratings.sql
--- postgres/tables/comments.sql
+++ postgres/tables/comments.sql
@@ -1,6 +1,9 @@
 create table comments (
 	id bigserial primary key,
 
+	course_id bigint references courses not null,
+
 	title text,
-	content text
+	content text,
+	created_at timestamp with time zone
 );
