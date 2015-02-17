57942d0f3bc24fb5e0e92f6a50ef99f995d39265
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-02-17 11:15:16 +0200

    Added more test data to import.sql

--- postgres/import.sql
+++ postgres/import.sql
@@ -1,3 +1,10 @@
 insert into users (username, email, password) values
 ('admin', 'admin@mail.com', 'password'),
 ('user', 'user@mail.com', 'password');
+
+insert into courses (name, description) values
+('Super Course 1', 'This is description to course 1'),
+('English', 'This is description to english');
+
+insert into comments (course_id, title, content, created_at) values
+(1, 'This is my first comment', 'This is the test content of the comments', now());
